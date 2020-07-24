75d391f3f42528738e0584b0f8b4ed0d81d69a0e
liuguang <67579722@qq.com>
2020-07-24 16:52:00 +0800

    add角色数据修改

--- TlbbGmTool/ViewModels/EditRoleWindowViewModel.cs
+++ TlbbGmTool/ViewModels/EditRoleWindowViewModel.cs
@@ -1,54 +1,39 @@
-﻿using TlbbGmTool.Models;
+﻿using TlbbGmTool.Core;
+using TlbbGmTool.Models;
 using TlbbGmTool.View.Windows;
 
 namespace TlbbGmTool.ViewModels
 {
-    public class EditRoleWindowViewModel : GameRole
+    public class EditRoleWindowViewModel : BindDataBase
     {
         #region Fields
 
-        private MainWindowViewModel _mainWindowViewModel;
-        private GameRole _gameRole;
+        public MainWindowViewModel MainWindowViewModel { get; private set; }
+        public GameRole GameRole{ get; private set; }
         private EditRoleWindow _editRoleWindow;
+
+        #endregion
+
+        #region Properties
+
+        public string WindowTitle => $"管理 {GameRole.Charname}(角色id: {GameRole.Charguid})";
 
         #endregion
 
         public void InitData(MainWindowViewModel mainWindowViewModel, GameRole gameRole,
             EditRoleWindow editRoleWindow)
         {
-            _mainWindowViewModel = mainWindowViewModel;
-            _gameRole = gameRole;
+            MainWindowViewModel = mainWindowViewModel;
+            GameRole = gameRole;
             _editRoleWindow = editRoleWindow;
-            //初始化属性
-            Aid = gameRole.Aid;
-            Accname = gameRole.Accname;
-            Charguid = gameRole.Charguid;
-            Charname = gameRole.Charname;
-            Title = gameRole.Title;
-            Menpai = gameRole.Menpai;
-            Level = gameRole.Level;
-            Scene = gameRole.Scene;
-            Xpos = gameRole.Xpos;
-            Zpos = gameRole.Zpos;
-            Hp = gameRole.Hp;
-            Mp = gameRole.Mp;
-            Str = gameRole.Str;
-            Spr = gameRole.Spr;
-            Con = gameRole.Con;
-            Ipr = gameRole.Ipr;
-            Dex = gameRole.Dex;
-            Points = gameRole.Points;
-            Enegry = gameRole.Enegry;
-            Energymax = gameRole.Energymax;
-            Vigor = gameRole.Vigor;
-            Maxvigor = gameRole.Maxvigor;
-            Exp = gameRole.Exp;
-            Pkvalue = gameRole.Pkvalue;
-            Vmoney = gameRole.Vmoney;
-            Bankmoney = gameRole.Bankmoney;
-            Yuanbao = gameRole.Yuanbao;
-            Menpaipoint = gameRole.Menpaipoint;
-            Zengdian = gameRole.Zengdian;
+        }
+
+        /// <summary>
+        /// 数据修改后,通知标题进行更新
+        /// </summary>
+        public void NotifyWindowTitleChange()
+        {
+            RaisePropertyChanged(nameof(WindowTitle));
         }
     }
 }
